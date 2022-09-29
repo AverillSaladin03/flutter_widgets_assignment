d4276f4330920eed67af285acb05defb6843de16
Averill Saladin <76471341+AverillSaladin03@users.noreply.github.com>
2022-09-30 00:25:47 +0700

    maybe done, commit #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets_assignment_1/view/pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        
-      ),
+      debugShowCheckedModeBanner: false,
+      home: homePage()
     );
   }
 }
